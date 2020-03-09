151067cf91af21c75b2d15ed1f63297acc2c53ce
jeromefirlej <tintale@free.fr>
2020-03-09 10:51:04 +0100

    Add fizzbuzz correction

---
+++ FizzBuzz/src/FizzBuzzClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzzImpl
+{
+    public class FizzBuzzClass
+    {
+        public string FizzBuzz(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            if (number  % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            return number.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/tests/FizzBuzzShould.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+using FizzBuzzImpl;
+
+namespace FizzBuzzImpl.Tests
+{
+    public class FizzBuzzClass_FizzBuzzShould
+    {
+        private FizzBuzzClass fizz;
+        public FizzBuzzClass_FizzBuzzShould()
+        {
+            fizz = new FizzBuzzClass();
+        }
+        
+        [Fact]
+        public void Return2_WhenEnter2()
+        {
+            //Arrange
+            int value  = 2;
+            string expected = "2";
+            
+            //Act
+            string result = fizz.FizzBuzz(value);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+        
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void ReturnFizz_WhenEnter3(int value)
+        {
+            //Arrange
+            string expected = "Fizz";
+            
+            //Act
+            string result = fizz.FizzBuzz(value);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void ReturnBuzz_WhenEnter5()
+        {
+            //Arrange
+            int value = 5;
+            string expected = "Buzz";
+            
+            //Act
+            string result = fizz.FizzBuzz(value);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+        
+        [Fact]
+        public void ReturnFizzBuzz_WhenEnter15()
+        {
+            //Arrange
+            int value = 15;
+            string expected = "FizzBuzz";
+            
+            //Act
+            string result = fizz.FizzBuzz(value);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+        
+    }
+}
